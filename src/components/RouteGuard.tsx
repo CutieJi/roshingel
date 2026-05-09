297eedd0b75772bc14f543df65f586a61ffa6b1b
Rosh <roshjingel@gmail.com>
2026-05-10 00:40:28 +0800

    UPDATED

--- src/components/RouteGuard.tsx
+++ src/components/RouteGuard.tsx
@@ -92,7 +92,7 @@ const RouteGuard: React.FC<RouteGuardProps> = ({ children }) => {
     return (
       <Column paddingY="128" maxWidth={24} gap="24" center>
         <Heading align="center" wrap="balance">
-          This page is password protected
+          This page is password protected pass:cutieji
         </Heading>
         <Column fillWidth gap="8" horizontal="center">
           <PasswordInput
